3f799129a05a427193503da036b6c613486cb3ee
chen.jingyi6 <adelejychen@gmail.com>
2024-11-24 19:18:00 -0800

    update openai func

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import redirect  # Import redirect
-from myproject import views  # Import your views
+from myproject import chatbox, views  # Import your views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +27,7 @@ urlpatterns = [
     path('profile/', views.register),
     path('profile/edit/', views.register),
     path('', lambda request: redirect('home/', permanent=False)),
-    path('chatbox/', views.chatbox, name='chatbox'),
+    path('chatbox/', chatbox.chatbox_view, name='chatbox'),
 
 ]
 
