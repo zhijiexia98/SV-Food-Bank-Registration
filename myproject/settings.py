e74942e226003500e0f64c7dfafdaeb9f43b995d
chen.jingyi6 <adelejychen@gmail.com>
2024-12-04 11:28:27 -0800

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from dotenv import load_dotenv
 
+
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
